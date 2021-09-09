436cd53d0bb40153698aac25d46b5ae6f1ea095a
AwKMelChan <zoro.roronoa.manga@gmail.com>
2021-09-09 10:49:56 +0200

    feat [cartOrder]: get address from a form. \n Todo:need to connect to the api

--- src/main/webapp/app/cart-order/cart-order.module.ts
+++ src/main/webapp/app/cart-order/cart-order.module.ts
@@ -3,9 +3,10 @@ import { CommonModule } from '@angular/common';
 
 import { CartOrderRoutingModule } from './cart-order-routing.module';
 import { CartOrderComponent } from './cart-order.component';
+import { FormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [CartOrderComponent],
-  imports: [CommonModule, CartOrderRoutingModule],
+  imports: [CommonModule, CartOrderRoutingModule, FormsModule],
 })
 export class CartOrderModule {}
