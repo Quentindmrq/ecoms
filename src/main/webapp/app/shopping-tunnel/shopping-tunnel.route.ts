f6c0c1cde27ffccb6bbb8939533a1543754ba494
Zefuri <ciprianu98@gmail.com>
2021-09-09 16:41:18 +0200

    WIP[shopping-tunnel]: routing TODO + stepper TODO

---
+++ src/main/webapp/app/shopping-tunnel/shopping-tunnel.route.ts
@@ -0,0 +1,8 @@
+import { Route } from '@angular/router';
+
+import { TunnelStepperComponent } from './tunnel-stepper/tunnel-stepper.component';
+
+export const SHOPPING_TUNNEL_ROUTE: Route = {
+  path: '',
+  component: TunnelStepperComponent,
+};
