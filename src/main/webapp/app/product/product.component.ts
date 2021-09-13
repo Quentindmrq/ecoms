d2523bc52b353a2e862f7380244118eaf11037bc
Axel Nowak <garmemail@yahoo.fr>
2021-09-13 15:16:11 +0200

    feat[productPage]: retrives product and basic html

--- src/main/webapp/app/product/product.component.ts
+++ src/main/webapp/app/product/product.component.ts
@@ -1,5 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute, Params } from '@angular/router';
+import { CartService } from 'app/cart/cart.service';
+import { StockService } from 'app/entities/stock/service/stock.service';
+import { Stock } from 'app/entities/stock/stock.model';
 
 @Component({
   selector: 'jhi-product',
@@ -8,11 +11,43 @@ import { ActivatedRoute, Params } from '@angular/router';
 })
 export class ProductComponent implements OnInit {
   productId: number;
-  constructor(private activatedRoute: ActivatedRoute) {}
+  stock: Stock | null;
+  loading: boolean;
+  error: any;
+  constructor(private activatedRoute: ActivatedRoute, private stockService: StockService, private cartService: CartService) {}
 
   ngOnInit(): void {
-    this.activatedRoute.params.subscribe((params: Params) => {
-      this.productId = params['id'];
-    });
+    this.loading = true;
+    this.activatedRoute.params.subscribe(
+      (params: Params) => {
+        this.productId = params['id'];
+        this.getProduct();
+      },
+      error => {
+        this.error = error;
+        this.loading = false;
+      }
+    );
+  }
+
+  getProduct(): void {
+    this.stockService.find(this.productId).subscribe(
+      stockRes => {
+        this.stock = stockRes.body;
+        this.loading = false;
+      },
+      error => {
+        this.error = error;
+        this.loading = false;
+      }
+    );
+  }
+
+  addToCart(): void {
+    if (this.stock?.product) {
+      this.cartService.addToCart(this.stock.product);
+    } else {
+      this.error = "Can't add to cart, product null or undef";
+    }
   }
 }
--- src/main/webapp/app/product/product.module.ts
+++ src/main/webapp/app/product/product.module.ts
@@ -3,15 +3,10 @@ import { CommonModule } from '@angular/common';
 
 import { ProductRoutingModule } from './product-routing.module';
 import { ProductComponent } from './product.component';
-
+import { SharedModule } from 'app/shared/shared.module';
 
 @NgModule({
-  declarations: [
-    ProductComponent
-  ],
-  imports: [
-    CommonModule,
-    ProductRoutingModule
-  ]
+  declarations: [ProductComponent],
+  imports: [CommonModule, SharedModule, ProductRoutingModule],
 })
-export class ProductModule { }
+export class ProductModule {}
