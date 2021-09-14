a09a498e25300e72f67d8ed59da025842c670eba
Zefuri <ciprianu98@gmail.com>
2021-09-14 14:18:34 +0200

    WIP [shopping-tunnel] : 2 forms done + beginning of validation

--- src/main/webapp/app/shopping-tunnel/shopping-tunnel.component.ts
+++ src/main/webapp/app/shopping-tunnel/shopping-tunnel.component.ts
@@ -2,6 +2,10 @@ import { Component, OnInit } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { CartService } from 'app/cart/cart.service';
 import { Address } from 'app/entities/address/address.model';
+import { AddressService } from 'app/entities/address/service/address.service';
+import { OrderLine } from 'app/entities/order-line/order-line.model';
+import { OrderLineService } from 'app/entities/order-line/service/order-line.service';
+import { OrderService } from 'app/entities/order/service/order.service';
 
 @Component({
   selector: 'jhi-shopping-tunnel',
@@ -12,19 +16,27 @@ export class ShoppingTunnelComponent implements OnInit {
   form1: FormGroup;
   form2: FormGroup;
 
-  model: Address;
+  address: Address;
 
-  months: number[];
+  error: any;
+
+  months: string[];
   years: number[];
 
-  constructor(private cartService: CartService, private _formBuilder: FormBuilder) {}
+  constructor(
+    private cartService: CartService,
+    private _formBuilder: FormBuilder,
+    private addressService: AddressService,
+    private orderService: OrderService,
+    private orderLineService: OrderLineService
+  ) {}
 
   ngOnInit(): void {
     const now = new Date();
-    this.months = new Array<number>();
+    this.months = new Array<string>();
     this.years = new Array<number>();
     for (let i = 0; i < 12; i++) {
-      this.months.push(i + 1);
+      this.months.push(String(i + 1).padStart(2, '0'));
       this.years.push(now.getFullYear() + i);
     }
 
@@ -45,7 +57,40 @@ export class ShoppingTunnelComponent implements OnInit {
       cardExpirationYear: ['', Validators.required],
       cardValidationCode: ['', Validators.required],
     });
-
-    this.model = new Address();
   }
+
+  // public createOrder(): void {
+  //   const firstName = this.form1.get('firstName')?.value;
+  //   const lastName = this.form1.get('lastName')?.value;
+  //   const address1 = this.form1.get('address1')?.value;
+  //   const address2 = this.form1.get('address2')?.value;
+  //   const country = this.form1.get('country')?.value;
+  //   const postalCode = this.form1.get('postalCode')?.value;
+  //   const city = this.form1.get('city')?.value;
+  //   this.address = new Address(undefined, firstName, lastName, country, postalCode, city, address1, address2);
+
+  //   // const cardName = this.form2.get('cardName')?.value;
+  //   // const cardNumber = this.form2.get('cardNumber')?.value;
+  //   // const cardExpirationMonth = this.form2.get('cardExpirationMonth')?.value;
+  //   // const cardExpirationYear = this.form2.get('cardExpirationYear')?.value;
+  //   // const cardValidationCode = this.form2.get('cardValidationCode')?.value;
+
+  //   let addressId: number | undefined;
+  //   this.addressService.create(this.address).subscribe(
+  //     addressRes => (addressId = addressRes.body?.id),
+  //     error => {
+  //       this.error = error;
+  //     }
+  //   );
+
+  //   let orderLineIds = Array<number>();
+  //   this.cartService.cart.subscribe(cart => {
+  //     cart.forEach(item => {
+  //       const orderLine = new OrderLine(undefined, item.quantity, item.product);
+  //       this.orderLineService.create(orderLine).subscribe(orderLineRes => {
+  //         orderLineIds.push;
+  //       });
+  //     });
+  //   });
+  // }
 }
