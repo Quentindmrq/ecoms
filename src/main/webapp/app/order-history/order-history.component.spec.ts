21990cfe622da8edbafc2938eca61181295b1975
Hytock <58024279+Hytock@users.noreply.github.com>
2021-09-15 15:03:28 +0200

    Order history (#7)
    * feat[cart]:
    nav to product pages and dynamic prices
    
    * WIP[orderHistory Page]:
    accordion with basic layout
    
    * feat[orderHistory]:
    finished layout
    
    * feat[cart]:
    discard
    
    * fix[orderHistoryspec]:
    fix imports

--- src/main/webapp/app/order-history/order-history.component.spec.ts
+++ src/main/webapp/app/order-history/order-history.component.spec.ts
@@ -1,5 +1,8 @@
 import { HttpClientModule } from '@angular/common/http';
 import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { RouterModule } from '@angular/router';
+import { JhMaterialModule } from 'app/shared/jhmaterial.module';
+import { TranslateDirective } from 'app/shared/language/translate.directive';
 
 import { OrderHistoryComponent } from './order-history.component';
 
@@ -9,8 +12,8 @@ describe('OrderHistoryComponent', () => {
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      declarations: [OrderHistoryComponent],
-      imports: [HttpClientModule],
+      declarations: [OrderHistoryComponent, TranslateDirective],
+      imports: [HttpClientModule, JhMaterialModule, RouterModule],
     }).compileComponents();
   });
 
--- src/main/webapp/app/order-history/order-history.module.ts
+++ src/main/webapp/app/order-history/order-history.module.ts
@@ -3,15 +3,10 @@ import { CommonModule } from '@angular/common';
 
 import { OrderHistoryRoutingModule } from './order-history-routing.module';
 import { OrderHistoryComponent } from './order-history.component';
-
+import { SharedModule } from 'app/shared/shared.module';
 
 @NgModule({
-  declarations: [
-    OrderHistoryComponent
-  ],
-  imports: [
-    CommonModule,
-    OrderHistoryRoutingModule
-  ]
+  declarations: [OrderHistoryComponent],
+  imports: [CommonModule, SharedModule, OrderHistoryRoutingModule],
 })
-export class OrderHistoryModule { }
+export class OrderHistoryModule {}
