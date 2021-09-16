2294127398edebce6c72450843d59fa1e511b19a
Zefuri <ciprianu98@gmail.com>
2021-09-16 10:59:40 +0200

    merge master

---
+++ src/main/java/om/cgi/formation/jhipster/ecom/domain/enumeration/Region.java
@@ -0,0 +1,13 @@
+package om.cgi.formation.jhipster.ecom.domain.enumeration;
+
+/**
+ * The Region enumeration.
+ */
+public enum Region {
+    EUW,
+    NA,
+    KR,
+    EUNE,
+    OCE,
+    JP,
+}
