cc0211facadb4e48560094569cc6a6a6cb094751
Axel Nowak <garmemail@yahoo.fr>
2021-09-20 12:52:41 +0200

    feat[delete dialog]: added delete dialg for cart items

--- src/main/webapp/app/cart/cart.component.ts
+++ src/main/webapp/app/cart/cart.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { MatDialog } from '@angular/material/dialog';
 import { Router } from '@angular/router';
+import { DeleteDialogComponent } from 'app/delete-dialog/delete-dialog.component';
 import { Product } from 'app/entities/product/product.model';
 import { CartItem, CartService } from './cart.service';
 
@@ -11,7 +13,7 @@ import { CartItem, CartService } from './cart.service';
 export class CartComponent implements OnInit {
   cartItems: CartItem[];
 
-  constructor(private cartService: CartService, private router: Router) {
+  constructor(private cartService: CartService, private router: Router, public dialog: MatDialog) {
     // donothing
   }
 
@@ -42,5 +44,21 @@ export class CartComponent implements OnInit {
   validate(): void {
     window.console.debug('cart-validate');
     this.router.navigate(['/shopping-tunnel']);
+  }
+
+  openDeleteDialog(product: Product): void {
+    const dialogRef = this.dialog.open(DeleteDialogComponent, {
+      data: {
+        content: `Are ou sure you want to delete ${product.name ? product.name : 'this product'} from your cart ?`,
+        trueButton: 'Delete',
+        falseButton: 'Cancel',
+      },
+    });
+
+    dialogRef.afterClosed().subscribe(result => {
+      if (result === true) {
+        this.deleteFromCart(product);
+      }
+    });
   }
 }
