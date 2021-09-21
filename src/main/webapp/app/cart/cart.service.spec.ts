623a75752b2cb1bdf5ff1e35d068db8f63c9363f
Zefuri <ciprianu98@gmail.com>
2021-09-21 09:57:24 +0200

    test [CartService] : testing CartService

--- src/main/webapp/app/cart/cart.service.spec.ts
+++ src/main/webapp/app/cart/cart.service.spec.ts
@@ -7,19 +7,58 @@ import { TranslateModule } from '@ngx-translate/core';
 import { NgxWebstorageModule } from 'ngx-webstorage';
 // import { RouterModule } from '@angular/router';
 import { RouterTestingModule } from '@angular/router/testing';
+import { Order } from 'app/entities/order/order.model';
+import { OrderLine } from 'app/entities/order-line/order-line.model';
+import { HttpClientTestingModule, HttpTestingController } from '@angular/common/http/testing';
+import { Product } from 'app/entities/product/product.model';
 
 describe('CartService', () => {
   let service: CartService;
+  let expectedResult: OrderLine[] | Order | boolean | number | null;
+  let httpMock: HttpTestingController;
 
   beforeEach(() => {
     TestBed.configureTestingModule({
       declarations: [TranslateDirective],
-      imports: [HttpClientModule, RouterTestingModule.withRoutes([]), TranslateModule.forRoot(), NgxWebstorageModule.forRoot()],
+      imports: [
+        HttpClientTestingModule,
+        HttpClientModule,
+        RouterTestingModule.withRoutes([]),
+        TranslateModule.forRoot(),
+        NgxWebstorageModule.forRoot(),
+      ],
     });
+    expectedResult = null;
+    httpMock = TestBed.inject(HttpTestingController);
     service = TestBed.inject(CartService);
   });
 
   it('should be created', () => {
     expect(service).toBeTruthy();
+  });
+
+  describe('Service methods', () => {
+    it('should have added one orderLine', () => {
+      service.cart.subscribe(cartOrder => (expectedResult = cartOrder));
+      service.addToCart(new Product(123), 3);
+      expect(service.numberOfItems).toEqual(3);
+      expect((expectedResult as Order).orderLines?.length).toEqual(1);
+    });
+
+    it('should be null after discard', () => {
+      service.cart.subscribe(cartOrder => (expectedResult = cartOrder));
+      service.addToCart(new Product(123), 3);
+      service.discard();
+      expect(expectedResult).toEqual(null);
+    });
+
+    it('should be null after discard', () => {
+      service.cart.subscribe(cartOrder => (expectedResult = cartOrder));
+      const prod = new Product(123);
+      service.addToCart(prod, 3);
+      service.removeOneFromCart(prod);
+      expect(service.numberOfItems).toEqual(2);
+      expect((expectedResult as Order).orderLines?.length).toEqual(1);
+    });
   });
 });
